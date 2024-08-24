1527f20fdcec7780355f015f00983e9bdeb90b51
Sumit Samantray <162700659+sumitsamantray@users.noreply.github.com>
2024-08-24 15:35:55 +0530

    Add files via upload

---
+++ Practice sql 1.sql
@@ -0,0 +1,5 @@
+select * from moviesdb.movies where industry ='bollywood';
+select distinct industry from moviesdb.movies;
+select * from moviesdb.movies where title like 'thor%';
+select * from moviesdb.movies where title like '%america%';
+select * from moviesdb.movies;
